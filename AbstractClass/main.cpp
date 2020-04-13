0e4105fdba18ac7a8978bd7dba102aaadbf49a37
Cedric Lemaitre <c.lemaitre58@gmail.com>
2020-04-13 14:36:04 +0200

    add example for non pure class in the main

--- AbstractClass/main.cpp
+++ AbstractClass/main.cpp
@@ -7,9 +7,13 @@ int main() {
 
     abstract_class::Child my_child;
     my_child.set_a(45);
-    std::cout << "Value for attribute a : " << my_child.get_a() << std::endl;
+    std::cout << "Value for attribute a (pure): " << my_child.get_a() << std::endl;
 
     //    abstract_class::Parent my_parent;
     abstract_class_non_pure::Parent my_parent;
+
+    abstract_class_non_pure::Child my_child_non_pure;
+    my_child_non_pure.set_a(69);
+    std::cout << "Value for attribute a (non pure) : " << my_child_non_pure.get_a() << std::endl;
     return 0;
 }
